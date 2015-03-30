6618be4573479be3466b68696a74b38dc69d67fb
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-30 18:34:56 -0400

    add migration file

---
+++ db/migrate/20150330222510_add_longitude_and_latitude_to_locations.rb
@@ -0,0 +1,6 @@
+class AddLongitudeAndLatitudeToLocations < ActiveRecord::Migration
+  def change
+    add_column :locations, :longitude, :float
+    add_column :locations, :latitude, :float
+  end
+end
