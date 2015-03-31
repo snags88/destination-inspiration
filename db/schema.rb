43ecd14f9b914b9b88e92dfb5167e9c0ba1ee2ea
rebecca-eakins <rebecca.a.eakins@gmail.com>
2015-03-31 16:18:03 -0400

    schema updated and data successfully seeded

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331140306) do
+ActiveRecord::Schema.define(version: 20150331191644) do
 
   create_table "attractions", force: :cascade do |t|
     t.string   "name"
