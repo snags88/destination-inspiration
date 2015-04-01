0275bec72376e438c39ccbd2ec9aa2ccc39abe43
Seiji Naganuma <Snaganumas@gmail.com>
2015-03-31 20:50:03 -0400

    refactor zilyo api class and seed method

--- app/models/lodging.rb
+++ app/models/lodging.rb
@@ -4,9 +4,10 @@ class Lodging < ActiveRecord::Base
 
   def self.seed_data(data_points)
     Location.all.each do |location|
-      zilyo = Zilyo.new(location, ["airbnb","hostelworld"], data_points)
-      zilyo.listings_data.each do |listing|
-        Lodging.find_or_create_by_zilyo_id(listing)
+      zilyo = Zilyo.new(location, ["airbnb","hostelworld"])
+      1.upto(20) do |page|
+        zilyo.referesh_dataset(page)
+        zilyo.listings_data.each {|listing| Lodging.find_or_create_by_zilyo_id(listing)}
       end
     end
   end
--- lib/zilyo.rb
+++ lib/zilyo.rb
@@ -6,12 +6,15 @@ class Zilyo
   API_HEADER = { "X-Mashape-Key" => ENV['mashape_key'],
                         "Accept" => "application/json"}
 
-  attr_accessor :result
+  attr_accessor :result, :page
 
-  def initialize(location, providers, num_of_results)
+  def initialize(location, providers)
     @location = location
     @providers = providers
-    @num_of_results = num_of_results
+  end
+
+  def referesh_dataset(page)
+    @page = page
     api_call
     set_json
   end
@@ -24,11 +27,10 @@ class Zilyo
     @result = JSON.load(@response.raw_body)["result"]
   end
 
-
   def listings_data
     @result.collect do |listing|
-      listing_hash(listing)
-    end
+      filtered_listing(listing_hash(listing))
+    end.compact
   end
 
   def listing_hash(listing)
@@ -38,14 +40,25 @@ class Zilyo
       "num_of_beds" => listing["attr"]["beds"],
       "lodging_type" => listing["attr"]["propType"]["text"],
       "booking_url" => listing["provider"]["url"],
-      "currency_id" => "USD",
+      "currency_id" => @location.currency.id,
       "location_id" => @location.id,
       "zilyo_id" => listing["id"]}
   end
 
+  def filtered_listing(hash)
+    if hash["num_of_beds"] > 8
+      nil
+    elsif hash["lodging_type"] == "Hostel"
+      hash["num_of_beds"] = 1
+      hash
+    else
+      hash
+    end
+  end
+
   private
     def api_url
-      URL + [coordinates_string, max_distance, providers_string, results_per_page].join("&")
+      URL + [coordinates_string, max_distance, page_num, providers_string, results_per_page].join("&")
     end
 
     def coordinates_string
@@ -58,11 +71,15 @@ class Zilyo
     end
 
     def results_per_page
-      "resultsperpage=#{@num_of_results}"
+      "resultsperpage=50"
     end
 
     def max_distance
       "maxdistance=80"
+    end
+
+    def page_num
+      "page=#{@page}"
     end
 
 end
