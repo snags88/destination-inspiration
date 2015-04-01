af5be90ed915d23b3bef89cb01867fd2a88291f3
rebecca-eakins <rebecca.a.eakins@gmail.com>
2015-04-01 17:16:53 -0400

    show page tidy for wednesday demo

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,8 @@
 class Image < ActiveRecord::Base
   belongs_to :location
 
+  validates_uniqueness_of :url
+
   def self.seed_images
     Location.all.each do |location|
       Flickr.new(location, 12).gallery.each do |photo|
