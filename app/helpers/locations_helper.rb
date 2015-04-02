9a9d85e9d618174fda48cf2177e6dd6b114b54cb
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-02 17:22:47 -0400

    finish attractions styling and start seeding

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -2,6 +2,6 @@ module LocationsHelper
 
   def attraction_by_type(type)
     attractions = @location.attractions.where(:attraction_type => type)
-    render partial: "locations/attraction", collection: attractions, as: :attraction
+    render partial: "locations/attractions/show", collection: attractions, as: :attraction
   end
 end
