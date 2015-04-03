1b145447d3959a8f62a2d3274ad51dc2e275d1ee
Seiji Naganuma <Snaganumas@gmail.com>
2015-04-03 14:50:23 -0400

    fix precompile issue

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,6 +3,7 @@ class Attraction < ActiveRecord::Base
   belongs_to :currency
 
   validates :name, :description, :attraction_type, presence: true
+  validates :name, :uniqueness => true
 
   def self.find_by_params(params)
     self.find_by(:id => params[:id], :location_id => params[:location_id])
